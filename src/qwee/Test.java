9a55bba04274f060e737de675f943f3980da4c8a
lifeihong <1137131459>
2019-08-07 18:29:18 +0800

    ee

--- src/qwee/Test.java
+++ src/qwee/Test.java
@@ -4,7 +4,8 @@ public class Test {
 public static void main(String[] args) {
 	System.out.println("123");
 	System.out.println("1231");
-	
+	System.out.println("ttt");
+	System.out.println("666");
 		
 	
 }
